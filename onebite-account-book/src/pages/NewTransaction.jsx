6a5010d4ead6e90a82a4c749ae7d84bd57e9417c
smith6423 <ajh720@gmail.com>
2026-01-07 21:45:42 +0900

    day21 TransactionEditor

--- onebite-account-book/src/pages/NewTransaction.jsx
+++ onebite-account-book/src/pages/NewTransaction.jsx
@@ -1,9 +1,13 @@
 import TransactionEditor from "../components/TransactionEditor";
+import "./NewTransaction.css";
 
 export default function NewTransaction() {
   return (
-    <>
-      <TransactionEditor />
-    </>
+    <div className="NewTransaction">
+      <header>
+        <h1>새로운 기록</h1>
+      </header>
+      <TransactionEditor type={"CREATE"} />
+    </div>
   );
 }
